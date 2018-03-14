c294c030943e794c5e828af935f05a41193ff499
DESKTOP-B8G6NFV\user <purva.vansia@yahoo.com>
2018-03-14 14:31:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/user/testgitpush/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.user.testgitpush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void TestGit(){
+
+        int i = 1+1;
+    }
+}
